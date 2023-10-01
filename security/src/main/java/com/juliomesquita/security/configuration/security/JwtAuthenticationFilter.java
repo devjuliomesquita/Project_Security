9ebfd7af5c4ce0d46fd922c7c811f6908b24c09a
Julio C. Mesquita <juliocesarmcamilo@gmail.com>
2023-10-01 19:12:53 -0300

    WIP - JWTSERVICE - CREATE

---
+++ security/src/main/java/com/juliomesquita/security/configuration/security/JwtAuthenticationFilter.java
@@ -0,0 +1,35 @@
+package com.juliomesquita.security.configuration.security;
+
+import com.juliomesquita.security.service.security.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String tokenJwt = authHeader.substring(7);
+        final String userName;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+//            return;
+        }
+
+        userName = jwtService.extractUserName(tokenJwt);
+    }
+}
